6a926c2451d22e448b73941bf16b8c97a893e4c5
vkklt <vmarino1599@gmail.com>
2023-03-23 23:39:29 -0300

    Adicionando arquivo Python

--- jogodaVelha.py
+++ jogodaVelha.py
@@ -5,12 +5,14 @@ p1 = "X"
 p2 = "O"
 
 def imprimirTabuleiro():
-    for linha in tabuleiro:
-        for elemento in linha:
-            print (elemento, end = " ")
+    print ("-------------Tabuleiro-------------")
+    indices = [0,1,2]
+    print ("\t\t0\t1\t2")
+    for i in indices:
+        print("\t",i,"\t", end="")
+        for j in indices:
+            print (tabuleiro[i][j],"\t",end="")
         print()
-
-imprimirTabuleiro()
 
 def jogarMaquina():
     while True:
@@ -32,51 +34,56 @@ def verificarVencedor(tabuleiro):
         if linha[0] == linha[1] == linha[2] and linha[0] != 0 :
             if linha[0] == "X":
                 print("Você Venceu!")
-                imprimirTabuleiro()
+                x = 1
+                return x
             else: 
                 print("Você Perdeu!")
-                imprimirTabuleiro()
-            return exit()
-        
+                x = 2
+                return x
+      
     for coluna in range(3):
         if tabuleiro[0][coluna] == tabuleiro[1][coluna] == tabuleiro[2][coluna] and tabuleiro[0][coluna] != 0 :
             if tabuleiro[0][coluna] == "X":
                 print("Você Venceu!")
-                imprimirTabuleiro()
+                x = 1
+                return x
             else: 
                 print("Você Perdeu!")
-                imprimirTabuleiro()
-            return exit()
+                x = 2
+                return x
         
     if tabuleiro[0][0] == tabuleiro[1][1] == tabuleiro[2][2] and tabuleiro[0][0] != 0 :
         if tabuleiro [0][0] == "X":
             print("Você Venceu!")
-            imprimirTabuleiro()
+            x = 1
+            return x
         else: 
             print("Você Perdeu!")
-            imprimirTabuleiro()
-        return exit()
+            x = 2
+            return x
     
     if tabuleiro[0][2] == tabuleiro[1][1] == tabuleiro[2][0] and tabuleiro[0][2] != 0 :
         if tabuleiro[0][2] == "X":
             print("Você Venceu!")
-            imprimirTabuleiro()
+            x = 1
+            return x
         else: 
             print("Você Perdeu!")
-            imprimirTabuleiro()
-        return exit()
+            x = 2
+            return x
     
     ocorrencias = 0
     for lista in tabuleiro:
         ocorrencias += lista.count(0)
     if ocorrencias == 0:
         print("Empate!")
-        imprimirTabuleiro()
-        return exit()
+        x = 3
+        return x
 
 def jogar():
     while True:
         try:
+            imprimirTabuleiro()
             linha = int(input("Digite a linha: "))
             if linha < 0 or linha > 2:
                 print ("Linha inválida")
@@ -96,6 +103,35 @@ def jogar():
         
         jogarMaquina()
         
-        verificarVencedor(tabuleiro)
+        x = verificarVencedor(tabuleiro)
+        if x == 1 or x == 2 or x == 3:
+            return x
+        
 
-jogar()
+def main():
+    resultados = open("resultados.txt","a+")
+    print(" \n \n ---J O G O   D A   V E L H A--- ")
+    print("1 - Jogar")
+    print("2 - Resultados")
+    print("3 - Sair")
+
+    opcao = int(input())
+    if opcao == 1:
+        x = jogar()
+        if x == 1:
+            nome = input("Insira seu nome para registrar na lista de vencedores  ")
+            resultados.write(nome)
+            resultados.write("\n")
+        if x == 2:
+            resultados.write("CPU vencedora \n")
+        if x == 3:
+            resultados.write("Empate \n")
+    if opcao == 2:
+        resultados.seek(0)
+        resultado = resultados.read()
+        print(resultado)
+    if opcao == 3:
+        exit()
+
+main()
+    
